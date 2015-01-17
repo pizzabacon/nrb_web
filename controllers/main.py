a4598575d1630bb4ba844c0b8bb7434e9d00bb23
neshield <neshield@umich.edu>
2015-01-17 17:58:48 -0500

    Can't run app, but added parse query
    Login has users type their org name and submit. Goes to issues. Issues
    should query parse based on orgName, but I am unable to test this :(

--- controllers/main.py
+++ controllers/main.py
@@ -6,3 +6,8 @@ main = Blueprint('main', __name__, template_folder='views')
 def main_route():
     options = {}
     return render_template("index.html", **options)
+
+@app.route('/issues')
+def issues():
+	options = {}
+	return render_template("issues.html", **options)
